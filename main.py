4fd68bb80383ec6f66eea6a2c6bf7a3d94948561
Heizkoerper <navies.call.0j@icloud.com>
2022-11-27 20:36:30 +0100

    Added first outline

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print('Hello World!')
+count: int = 0
+iterations: int = 0
+
+
+def createSample(): print('create sample')
+
+
+def factorize(): print('factorize')
+
+
+# Compare the product calculated factors witht the original product
+def checkValidity(): print('check') 
+  
+
+def main(iterations):
+    
+    for count in range(iterations):
+        createSample()
+        factorize()
+        checkValidity()
+        
     
 
-if __name__ == '__main___':
-    main()
+if __name__ == '__main__':
+    
+    #iterations = int(input('Set number of iterations: '))
+    iterations = 1
+    
+    main(iterations)
